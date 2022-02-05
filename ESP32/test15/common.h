e1f94ebf49e8730a0a1beba320c603954b6d271e
Yoichi Tanibayashi <yoichi@tanibayashi.jp>
2022-02-05 23:35:23 +0900

    ADD: ESP32/test15/

---
+++ ESP32/test15/common.h
@@ -0,0 +1,22 @@
+/**
+ * Copyright (c) 2022 Yoichi Tanibayashi
+ */
+#ifndef _COMMON_H_
+#define _COMMON_H_
+
+#include <Arduino.h>
+#include <esp32-hal-log.h>
+
+/**
+ * Primary Mode
+ */
+typedef enum {
+              MODE_MAIN,
+              MODE_MENU,
+              MODE_SET_TEMP_OFFSET,
+              MODE_SET_WIFI,
+              MODE_N
+} Mode_t;
+static const String MODE_T_STR[] = {"Main", "Menu", "SetTempOffset", "SetWiFi"};
+
+#endif // _COMMON_H_
