ac09719392de4a0ad332a93207fd2146483d17b8
Yoichi Tanibayashi <yoichi@tanibayashi.jp>
2022-02-18 04:35:44 +0900

    START: ESP32/test34/

---
+++ ESP32/test34/Bme280.cpp
@@ -0,0 +1,65 @@
+/**
+ * Copyright (c) 2022 Yoichi Tanibayashi
+ */
+#include "Bme280.h"
+
+/**
+ *
+ */
+Bme280::Bme280(uint8_t addr, float temp_offset) {
+  this->_info.addr = addr;
+  this->_info.temp_offset = temp_offset;
+
+  this->_info.active = this->_bme.begin(this->_info.addr);
+
+  log_i("active=%d", this->_info.active);
+} // Bme280::Bme280()
+
+/**
+ *
+ */
+bool Bme280::is_active() {
+  return this->_info.active;
+} // Bme280::is_active();
+
+/**
+ *
+ */
+void Bme280::set_temp_offset(float temp_offset) {
+  this->_info.temp_offset = temp_offset;
+} // Bme280::set_temp_offset()
+
+/**
+ *
+ */
+float Bme280::get_temp_offset() {
+  return this->_info.temp_offset;
+} // Bme280::get_temp_offset()
+
+/**
+ * @param [out] info
+ */
+Bme280Info_t *Bme280::get() {
+  if ( ! this->_info.active ) {
+    log_w("active=%d", this->_info.active);
+    return NULL;
+  }
+
+  this->_info.temp = this->_bme.readTemperature() + this->_info.temp_offset;
+  this->_info.hum = this->_bme.readHumidity();
+  this->_info.pres = this->_bme.readPressure() / 100;
+
+  this->_info.thi = this->calc_thi(this->_info.temp, this->_info.hum);
+
+  log_i("%.1f C(%.1f) %.0f%% %0.0f hPa %.1f thi",
+        this->_info.temp, this->_info.temp_offset,
+        this->_info.hum, this->_info.pres, this->_info.thi);
+  return &this->_info;
+} // Bme280::get()
+
+/** static function
+ *
+ */
+float Bme280::calc_thi(float temp, float hum) {
+  return  0.81 * temp + 0.01 * hum * (0.99 * temp - 14.3) + 46.3;
+} // Bme280::calc_thi()
---
+++ ESP32/test34/ScanSsidMode.cpp
@@ -0,0 +1,134 @@
+/**
+ * Copyright (c) 2022 Yoichi Tanibayaishi
+ */
+#include "ScanSsidMode.h"
+
+static int count = 0;
+
+/** constructor
+ *
+ */
+ScanSsidMode::ScanSsidMode(String name, CommonData_t *common_data)
+  : ModeBase(name, common_data) {
+
+  this->ssidMenu = new OledMenu("SSIDs");
+  
+} // ScanSsidMode::ScanssidMode()
+
+/**
+ *
+ */
+bool ScanSsidMode::enter(Mode_t prev_mode) {
+  log_i("%s", this->get_name().c_str());
+
+  WiFi.scanNetworks(true, true);
+  count = 0;
+  
+  return true;
+} // ScanSsidMode::enter()
+
+/**
+ *
+ */
+bool ScanSsidMode::exit() {
+  log_i("%s", this->get_name().c_str());
+
+  // clear scan results
+  WiFi.scanDelete();
+  
+  // clear objects
+  for (int i=0; i < this->ment.size(); i++) {
+    delete this->ment[i];
+  }
+  this->ment.clear();
+  this->ment.shrink_to_fit();
+  
+  this->ssidMenu->clean();
+
+  return true;
+} // ScanSsidMode::exit()
+
+/**
+ *
+ */
+Mode_t ScanSsidMode::reBtn_cb(ButtonInfo_t *bi) {
+  if ( bi->click_count > 1 ) {
+    return MODE_MAIN;
+  }
+  if ( bi->click_count == 0 ) {
+    return MODE_N;
+  }
+
+  /*
+   * click_count == 1
+   */
+  if ( this->ssidMenu->ent.size() == 0 ) {
+    return MODE_N;
+  }
+  
+  OledMenuDst_t dst = this->ssidMenu->select();
+  log_i("dst.obj.text=\"%s\"", dst.obj.text);
+
+  common_data->netmgr_info->new_ssid = String(dst.obj.text);
+  log_i("common_data->netmgr_info->new_ssid=%s",
+        common_data->netmgr_info->new_ssid.c_str());
+  
+  return MODE_SET_SSID;
+} // ScanSsidMode::reBtn_cb()
+
+/**
+ *
+ */
+Mode_t ScanSsidMode::re_cb(RotaryEncoderInfo_t *ri) {
+  if ( ri->d_angle > 0 ) {
+    this->ssidMenu->cursor_up();
+    return MODE_N;
+  }
+  if ( ri->d_angle < 0 ) {
+    this->ssidMenu->cursor_down();
+    return MODE_N;
+  }
+  return MODE_N;
+} // ScanSsidMode::re_cb()
+
+/**
+ *
+ */
+void ScanSsidMode::display(Display_t *disp) {
+  int16_t ret = WiFi.scanComplete();
+
+  if ( ret < 0 ) {
+    disp->setFont(&FreeSans12pt7b);
+    disp->setTextSize(1);
+    disp->setCursor(10, 34);
+    disp->printf("Scanning");
+
+    disp->fillRect(0, 43, count, 4, WHITE);
+    count = (count + 1) % 128;
+
+    disp->setFont(NULL);
+    disp->setTextSize(1);
+    disp->setCursor(10,0);
+    disp->printf("ret=%d", ret);
+    
+    delay(100);
+    return;
+  }
+
+  if ( this->ssidMenu->ent.size() == 0 ) {
+    for (int i=0; i < ret; i++) {
+      if ( WiFi.SSID(i).length() == 0 ) {
+        log_i("SSID \"%s\": ignored", WiFi.SSID(i).c_str());
+        continue;
+      }
+      String ent_title = " " + WiFi.SSID(i) + " ";
+      String ent_text = WiFi.SSID(i);
+
+      this->ment.push_back(new OledMenuEnt(ent_title, ent_text.c_str()));
+      this->ssidMenu->addEnt(this->ment.back());
+    } // for(i)
+    return;
+  }
+
+  this->ssidMenu->display(disp);
+} // ScanSsidMode::display()
